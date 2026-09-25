911fb0d25c8c1d1a6a8833089e9c77b3c0972078
icekale <icekale@users.noreply.github.com>
2026-09-25 08:40:04 +0800

    chore: 发布 v1.12.269

--- app/version.py
+++ app/version.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 import json
 import time
 
-APP_VERSION = "1.12.268"
+APP_VERSION = "1.12.269"
 VERSION_CHECK_TTL = 6 * 3600  # 6 小时
 GITHUB_REPO = "icekale/vpush"
 
